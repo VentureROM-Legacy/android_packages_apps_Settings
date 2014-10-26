14720d84a9e1e4b37314757803228ec5e31dd025
Isaac Parker <isaacparker@gmail.com>
2014-10-26 17:45:32 +1000

    Fix "About Phone" crash
    Change-Id: I25293372b9b8f92be3e95639a6f2a9512d00143a

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -391,20 +391,22 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     }
 
     private boolean removePreferenceIfPackageNotInstalled(Preference preference) {
-        String intentUri = ((PreferenceScreen) preference).getIntent().toUri(1);
-        Pattern pattern = Pattern.compile("component=([^/]+)/");
-        Matcher matcher = pattern.matcher(intentUri);
+    	if(preference != null){
+    		String intentUri = ((PreferenceScreen) preference).getIntent().toUri(1);
+    		Pattern pattern = Pattern.compile("component=([^/]+)/");
+    		Matcher matcher = pattern.matcher(intentUri);
 
-        String packageName = matcher.find() ? matcher.group(1) : null;
-        if(packageName != null) {
-            try {
-                getPackageManager().getPackageInfo(packageName, 0);
-            } catch (NameNotFoundException e) {
-                Log.e(LOG_TAG,"package "+packageName+" not installed, hiding preference.");
-                getPreferenceScreen().removePreference(preference);
-                return true;
-            }
-        }
+    		String packageName = matcher.find() ? matcher.group(1) : null;
+    		if(packageName != null) {
+    			try {
+    				getPackageManager().getPackageInfo(packageName, 0);
+    			} catch (NameNotFoundException e) {
+    				Log.e(LOG_TAG,"package "+packageName+" not installed, hiding preference.");
+    				getPreferenceScreen().removePreference(preference);
+    				return true;
+    			}
+    		}
+    	}
         return false;
     }
 }
